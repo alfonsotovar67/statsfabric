297072100c1256f5d76bb4afdaec3ee56b3da910
alfonsotovar67 <alfonso.tovar67@gmail.com>
2024-11-04 09:41:48 -0600

    Version inicial del proyecto

---
+++ src/main/java/com/orioninc/statsfabric/StatsfabricApplication.java
@@ -0,0 +1,13 @@
+package com.orioninc.statsfabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsfabricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsfabricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orioninc/statsfabric/StatsfabricApplicationTests.java
@@ -0,0 +1,14 @@
+package com.orioninc.statsfabric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsfabricApplicationTests {
+
+	@Test
+	void contextLoads() throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+
+}
