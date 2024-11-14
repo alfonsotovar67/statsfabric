3f515540be2cc5f1395c5759d1484b82904eccf3
alfonsotovar67 <alfonso.tovar67@gmail.com>
2024-11-14 17:09:44 -0600

    Se modifico para travajar con POI

---
+++ src/main/java/com/orioninc/statsfabric/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.orioninc.statsfabric.constant;
+
+public class Constant {
+    public static final String DIRECTORY = "d://aeromexico//estadisticas//";
+
+    private Constant() {
+
+    }
+}
---
+++ src/main/java/com/orioninc/statsfabric/controller/dataLoad.java
@@ -0,0 +1,27 @@
+package com.orioninc.statsfabric.controller;
+
+import com.orioninc.statsfabric.constant.Constant;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RestController
+public class DataLoad {
+
+    @GetMapping("/upload")
+    public Boolean uploade(@RequestParam String file) {
+        try (FileInputStream fis = new FileInputStream(Constant.directory+file)) {
+            Workbook wb = new XSSFWorkbook(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+}
